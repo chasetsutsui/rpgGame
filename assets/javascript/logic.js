75cff6891336a46b64bdedf9b2ec399cb3bbe520
Chase Tsutsui <chasetsutsui@yahoo.com>
2020-06-20 13:20:21 -0700

    rendering character images with jquery

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,7 +4,52 @@ $(document).ready(function () {
             name: "Goku",
             health: 125,
             attack: 10,
-            imageUrl: ""
+            imageUrl: "assets/images/goku.jpg",
+            enemyAttackBack: 14
+        },
+        "Gohan": {
+            name: "Gohan",
+            health: 115,
+            attack: 14,
+            imageUrl: "assets/images/gohan.jpeg",
+            enemyAttackBack: 12
+        },
+        "Frieza": {
+            name: "Frieza",
+            health: 120,
+            attack: 11,
+            imageUrl: "assets/images/frieza.jpg",
+            enemyAttackBack: 24
+        },
+        "Majin Buu": {
+            name: "Majin Buu",
+            health: 140,
+            attack: 9,
+            imageUrl: "assets/images/buu.jpg",
+            enemyAttackBack: 19
+        }
+    };
+    console.log(characters);
+
+    var renderOne = function (character, renderArea) {
+        var charDiv = $("<div class= 'character' data-name= '" + character.name + "'>");
+        var charName = $("<div class='character-name'>").text(character.name);
+        var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
+        var charHealth = $("<div class='character-health'>").text(character.health);
+
+        charDiv.append(charName).append(charImage).append(charHealth);
+        $(renderArea).append(charDiv)
+    }
+
+    var renderCharacters = function (charObj, areaRender) {
+        if (areaRender === "#character-section") {
+            $(areaRender).empty();
+            for (var key in charObj) {
+                if (charObj.hasOwnProperty(key)) {
+                    renderOne(charObj[key], areaRender);
+                }
+            }
         }
     }
-})
+    renderCharacters(characters, "#character-section")
+});
