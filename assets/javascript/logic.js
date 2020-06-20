c80a4038ae1ed5d9b14f50d0e9888660897676ec
Chase Tsutsui <chasetsutsui@yahoo.com>
2020-06-20 09:54:51 -0700

    html, images, some CSS

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var characters = {
+        "Goku": {
+            name: "Goku",
+            health: 125,
+            attack: 10,
+            imageUrl: ""
+        }
+    }
+})
